11b42565c51e729e14f625835d7b84be31d737d5
Nikhil Kumar <nikhilsharma22092002@gmail.com>
2025-03-28 11:28:14 +0530

    Admin Dashboard Create

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Login from './components/Auth/Login'
+import AdminDashboard from './components/Dasboard/AdminDashboard'
 import EmployeeDasboard from './components/Dasboard/EmployeeDasboard'
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <>
       {/* <Login/> */}
       {/* <EmployeeDasboard/> */}
-      
+      <AdminDashboard/>
     </>
   )
 }
